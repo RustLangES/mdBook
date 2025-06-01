0fae3d8d2ebb67094ba7ea6813591c6cdfe78e19
Phosphorus-M <fernando.n.p.d.moscu@gmail.com>
2025-06-01 06:40:03 -0300

    fix: add serde rename for 2024 Rust edition in RustConfig

--- src/config.rs
+++ src/config.rs
@@ -671,6 +671,7 @@ pub struct RustConfig {
 /// Rust edition to use for the code.
 pub enum RustEdition {
     /// The 2024 edition of Rust
+    #[serde(rename = "2024")]
     E2024,
     /// The 2021 edition of Rust
     #[serde(rename = "2021")]
